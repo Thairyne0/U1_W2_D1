2ccdcd036a4ff36aecbc27574da5d0d03db3923a
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2024-10-07 13:10:20 +0200

    Update file.js

---
+++ JavaScript/file.js
@@ -0,0 +1,23 @@
+console.log("Qui dovrei scrivere Hello World ... Non lo faró\n");
+
+let nomeVariabileAutoesplicativo = "delle donne";
+
+let statoDAnimo = "paura";
+
+let variabileNumerica = 7;
+
+console.log("io ho " + statoDAnimo + " " + nomeVariabileAutoesplicativo);
+
+let variabileBooleano = true;
+
+console.log("sei sicuro che hai paura delle donne?");
+
+if (variabileBooleano) {
+  console.log("Si confermo ho paura delle donne");
+} else {
+  console.log("No non é vero");
+}
+
+alert("io ho " + statoDAnimo + " " + nomeVariabileAutoesplicativo);
+
+const CONSTANTE = 1;
